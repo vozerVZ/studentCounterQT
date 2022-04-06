87c51858669a1e9073a235f32fbb6a4a1aebd776
w3cassassin <pokir03@gmail.com>
2022-04-06 23:08:26 +0300

    Add insitute select

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,9 +23,7 @@ void MainWindow::loadTips()
 {
     vector<vector<std::string>> cities;
     db.readcsv("Caffe-and-cinema.csv", cities);
-    std::cout << "mas size " << cities.size() << std::endl;
     for(std::size_t i = 1; i < cities.size(); i++){
-        std::cout << cities[i][1] << std::endl;
         if(ui->comboBoxCity->findText(QString::fromStdString(cities[i][0])) == -1)
             ui->comboBoxCity->addItem(QString::fromStdString(cities[i][0]));
         if(ui->comboBoxCity->findText(QString::fromStdString(cities[i][1])) == -1)
@@ -34,6 +32,11 @@ void MainWindow::loadTips()
             ui->comboBoxCafe->addItem(QString::fromStdString(cities[i][2]));
         if(ui->comboBoxCity->findText(QString::fromStdString(cities[i][4])) == -1)
             ui->comboBoxCinema->addItem(QString::fromStdString(cities[i][4]));
+    }
+    db.readcsv("Institute.csv", cities);
+    for(std::size_t i = 1; i < cities.size(); i++){
+        if(ui->comboBoxCity->findText(QString::fromStdString(cities[i][1])) == -1)
+            ui->comboBoxCity->addItem(QString::fromStdString(cities[i][1]));
     }
 }
 
@@ -46,7 +49,7 @@ void MainWindow::on_pushButtonCalculate_clicked()
     std::string city = ui->comboBoxCity->currentText().toStdString();
     std::string homeAddress = ui->comboBoxDistrict->currentText().toStdString();
     std::string cinema = ui->comboBoxCinema->currentText().toStdString();
-    std::string institute = "MIREA";
+    std::string institute = ui->comboBoxInstitute->currentText().toStdString();
     std::string coffee = ui->comboBoxCafe->currentText().toStdString();
     int costs = student.getCosts(month, city, homeAddress, institute, cinema, coffee, db);
     QMessageBox msgBox;
